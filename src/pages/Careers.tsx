da1e66850e6e8c94942e974d411e4255b8719f6b
deweiiss <julian.weiss94@live.de>
2026-03-30 15:13:38 +0200

    FIX: jobcard fade in fix

--- src/pages/Careers.tsx
+++ src/pages/Careers.tsx
@@ -53,9 +53,9 @@ const Careers = () => {
 
     return (
       <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.3 }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.6 }}
         viewport={{ once: true }}
         className="group"
       >
